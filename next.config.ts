e3b5f1c450a78f1a1b74244b087937cbd994968f
Gyanendra Sahoo <gyanendrasahoo577@gmail.com>
2025-11-17 16:39:25 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   experimental: {
-    turbopackFileSystemChaceForDev: true,
+    turbopackFileSystemCacheForDev: true,
   } 
 };
 
